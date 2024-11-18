683cdc25e81bd1776723e1da7ef6d8b3dab048be
Arctic1ce <davariartin@gmail.com>
2024-11-18 11:16:21 -0800

    implemented checking for scanf and gets and printing out address of variable to be stored in

--- TaintAnalysis.cpp
+++ TaintAnalysis.cpp
@@ -6,6 +6,33 @@
 using namespace llvm;
 
 PreservedAnalyses TaintAnalysis::run(Function &F, FunctionAnalysisManager &AM) {
-    errs() << F.getName() << "\n";
+    errs() << "Analyzing function: " << F.getName() << "\n";
+    
+    for (auto &BB : F) {
+        for (auto &I : BB) {
+            if (CallInst *CI = dyn_cast<CallInst>(&I)) {
+                Function *CalledFunc = CI->getCalledFunction();
+                if (CalledFunc) {
+                    StringRef FuncName = CalledFunc->getName();
+
+                    // check for scaf and gets function calls
+                    if (FuncName == "scanf" || 
+                        FuncName == "gets") {
+                        errs() << "Found user input function: " << FuncName << " at ";
+                        I.getDebugLoc().print(errs());
+                        errs() << "\n";
+
+                        // for scanf, the first argument is the format string, subsequent arguments are pointers
+                        // for gets, the first and only argument is the destination buffer
+                        Value *DestAddr = CI->getArgOperand(FuncName == "scanf" ? 1 : 0);
+                        errs() << "Destination address: ";
+                        DestAddr->print(errs());
+                        errs() << "\n";
+                    }
+                }
+            }
+        }
+    }
+    
     return PreservedAnalyses::all();
 }
